35f5a002d8c4b6b13e2ee2026204ee9cddcd72e6
camiloduquee <camiloduquee@gmail.com>
2023-04-22 13:13:47 -0500

    terminada tarea

---
+++ src/components/Cards.jsx
@@ -0,0 +1,20 @@
+import Card from "./Card";
+
+export default function Cards(props) {
+  const { characters } = props;
+
+  return (
+    <div>
+      {characters.map((character, index) => (
+        <Card
+          key={index}
+          name={character.name}
+          species={character.species}
+          gender={character.gender}
+          image={character.image}
+          onClose={() => window.alert("Emulamos que se cierra la card")}
+        />
+      ))}
+    </div>
+  );
+}
