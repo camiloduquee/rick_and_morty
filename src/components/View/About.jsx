d9f8db3b94df1d36dc4d5463b473b7a6205b8f56
camiloduquee <camiloduquee@gmail.com>
2023-05-12 23:28:54 -0500

    arreglo body

--- src/components/View/About.jsx
+++ src/components/View/About.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import AboutTx from '../AboutTx/AboutTx.jsx';
-
+import Star from "../Star/Star.jsx";
 const About = () => {
     return(
         <div>
             <AboutTx />
+            <Star />
         </div>
     )
 }
